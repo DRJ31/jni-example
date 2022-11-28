663f2b38019d578e2091c13df80da88bc5198442
Renjie Deng <dengrenjie31@gmail.com>
2022-11-28 23:06:28 +0800

    Change sth

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@
 int main() {
     auto msg = showSms("114514");
     std::cout << "短信：" << msg << std::endl;
-    int ret = sendCode("13533353653", "114514");
+    int ret = sendCode("11451419198", "114514");
     return ret;
 }
